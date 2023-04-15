c74989a0a22e942b8c7ba6887c6a9a5f18d02afa
马萨卡 <yuzhe5671@gmail.com>
2023-04-15 17:16:08 +0800

    delete unused code

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -9,21 +9,16 @@
  * @see https://www.electron.build/configuration/configuration
  */
 module.exports = async function () {
-  const {getVersion} = await import('./version/getVersion.mjs');
+    const { getVersion } = await import('./version/getVersion.mjs')
 
-  return {
-    directories: {
-      output: 'dist',
-      buildResources: 'buildResources',
-    },
-    files: ['packages/**/dist/**'],
-    extraMetadata: {
-      version: getVersion(),
-    },
-
-    // Specify linux target just for disabling snap compilation
-    linux: {
-      target: 'deb',
-    },
-  };
-};
+    return {
+        directories: {
+            output: 'dist',
+            buildResources: 'buildResources',
+        },
+        files: ['packages/**/dist/**'],
+        extraMetadata: {
+            version: getVersion(),
+        },
+    }
+}
--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -1,6 +1,1 @@
-/**
- * @module preload
- */
-
-export {sha256sum} from './nodeCrypto';
-export {versions} from './versions';
+export { versions } from './versions'
