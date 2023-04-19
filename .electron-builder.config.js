ff635a1285d691911cc5eb68fa739781c437c8e0
马萨卡 <yuzhe5671@gmail.com>
2023-04-19 14:54:32 +0800

    md5 app.asar after electron build pack

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -1,9 +1,8 @@
-/**
- * TODO: Rewrite this config to ESM
- * But currently electron-builder doesn't support ESM configs
- * @see https://github.com/develar/read-config-file/issues/10
- */
-
+const crypto = require('crypto')
+const fs = require('fs')
+const fsPromise = require('fs/promises')
+const path = require('path')
+const { promisify } = require('util')
 /**
  * @type {() => import('electron-builder').Configuration}
  * @see https://www.electron.build/configuration/configuration
@@ -26,11 +25,42 @@ module.exports = async function () {
         win: {
             target: 'nsis',
         },
+
         nsis: {
             oneClick: false,
             perMachine: true,
             installerLanguages: 'zh_CN',
             allowToChangeInstallationDirectory: true,
+        },
+        async afterPack(context) {
+            const name =
+                process.env.npm_package_productName ||
+                process.env.npm_package_name
+            let dir = context.appOutDir
+            const platform = context.electronPlatformName
+            if (platform === 'darwin') {
+                dir = path.resolve(
+                    dir,
+                    `${name}.app`,
+                    'Contents',
+                    'Resources',
+                    'app.asar'
+                )
+            }
+            if (platform === 'win32') {
+                dir = path.resolve(dir, 'resources', 'app.asar')
+            }
+            const md5 = crypto
+                .createHash('md5')
+                .update(fs.readFileSync(dir))
+                .digest('hex')
+            await fsPromise.writeFile(
+                path.resolve(context.outDir, 'update-md5.txt'),
+                md5
+            )
+            await fsPromise.cp(dir, path.resolve(context.outDir, 'app.asar'), {
+                force: true,
+            })
         },
     }
 }
